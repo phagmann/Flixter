577c3af11ed345928bc6fd36d5d202aafb9ab89f
Patrick Hagmann <phagmann1@gmail.com>
2016-10-16 04:11:53 +0000

    finishing touches

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,22 +1,35 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
-  def new
-    @section = Section.new
-  end
 
   def create
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'ok!'
+  end
+
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
+  end
   
   def require_authorized_for_current_course
     if current_course.user != current_user
       render text: "Unauthorized", status: :unauthorized
     end
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
   end
 
 
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,13 @@
 class Section < ActiveRecord::Base
-    belongs_to :course
-    has_many :lessons
-    has_many :images
+  belongs_to :course
+  has_many :lessons
+  has_many :images
+  include RankedModel
+  ranks :row_order, :with_same => :course_id
+
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+
+    return section
+  end
 end
